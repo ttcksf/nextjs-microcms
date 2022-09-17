cee47d2c625f63d6a6dce46c8c8f08ae8c797ac2
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-09-18 00:04:18 +0900

    first

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { client } from "../libs/client";
+import styles from "../styles/Home.module.scss";
+
+//SSG
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blog" });
+  console.log(data);
+
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
+
+export default function Home({ blog }) {
+  return (
+    <div className={styles.container}>
+      {blog.map((blog) => (
+        <li key={blog.id}>
+          <Link href={`blog/${blog.id}`}>
+            <a href="">{blog.title}</a>
+          </Link>
+        </li>
+      ))}
+    </div>
+  );
+}
